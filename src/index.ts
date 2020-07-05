b90941869a0d364416b9a669d407e7a596901741
Daniel Viglione <dviglione02@gmail.com>
2020-07-04 21:37:33 -0400

    testing blocking vs nonblocking with ab

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+const app = express();
+import fs from 'fs';
+const fsp = fs.promises;
+const HOST = '0.0.0.0';
+const PORT = 8080;
+
+app.get('/async', async (req, res) => {
+    const data = await fsp.readFile('data/file.csv');
+
+    res.send({
+        statusCode: 200,
+        body: { 
+            resp: data.toString('utf8').slice(0,10),
+            success: true
+        }
+    })
+});
+
+app.get('/sync', (req, res) => {
+    const data = fs.readFileSync('data/file.csv');
+
+    res.send({
+        statusCode: 200,
+        body: { 
+            resp: data.toString('utf8').slice(0,10),
+            success: true
+        }
+    })
+});
+
+app.listen(PORT, HOST, () => {
+    console.log(
+      `Server ready at http://${HOST}:${PORT}`);
+});
